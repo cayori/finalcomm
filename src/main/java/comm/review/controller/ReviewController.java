80cfbe58ae18f622a70af79992947387776a1df9
KH_I\user1 <cayori@gmail.com>
2018-01-08 15:07:58 +0900

    no message

---
+++ src/main/java/comm/review/controller/ReviewController.java
@@ -0,0 +1,104 @@
+package comm.review.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import comm.common.common.CommandMap;
+import comm.review.service.ReviewService;
+
+@Controller
+@RequestMapping(value="/comm/review")
+public class ReviewController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="reviewService")
+	private ReviewService reviewService;
+	
+	@RequestMapping(value="/openBoardList.do")
+    public ModelAndView openBoardList(CommandMap commandMap) throws Exception{
+    	ModelAndView mv = new ModelAndView("/sample/boardList");
+    	
+    	return mv;
+    }
+	
+	@RequestMapping(value="/sample/selectBoardList.do")
+    public ModelAndView selectBoardList(CommandMap commandMap) throws Exception{
+    	ModelAndView mv = new ModelAndView("jsonView");
+    	
+    	List<Map<String,Object>> list = reviewService.selectBoardList(commandMap.getMap());
+    	mv.addObject("list", list);
+    	if(list.size() > 0){
+    		mv.addObject("TOTAL", list.get(0).get("TOTAL_COUNT"));
+    	}
+    	else{
+    		mv.addObject("TOTAL", 0);
+    	}
+    	
+    	return mv;
+    }
+	
+	@RequestMapping(value="/sample/openBoardWrite.do")
+	public ModelAndView openBoardWrite(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardWrite");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/insertBoard.do")
+	public ModelAndView insertBoard(CommandMap commandMap, HttpServletRequest request) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
+		
+		reviewService.insertBoard(commandMap.getMap(), request);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/openBoardDetail.do")
+	public ModelAndView openBoardDetail(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardDetail");
+		
+		Map<String,Object> map = reviewService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map.get("map"));
+		mv.addObject("list", map.get("list"));
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/openBoardUpdate.do")
+	public ModelAndView openBoardUpdate(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardUpdate");
+		
+		Map<String,Object> map = reviewService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map.get("map"));
+		mv.addObject("list", map.get("list"));
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/updateBoard.do")
+	public ModelAndView updateBoard(CommandMap commandMap, HttpServletRequest request) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardDetail.do");
+		
+		reviewService.updateBoard(commandMap.getMap(), request);
+		
+		mv.addObject("IDX", commandMap.get("IDX"));
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/deleteBoard.do")
+	public ModelAndView deleteBoard(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
+		
+		reviewService.deleteBoard(commandMap.getMap());
+		
+		return mv;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<html>
+<head><title>index</title></head>
+<body>
+
+<a href="/finalcomm/sample/openBoardList.do">여행후기</a>
+<br><br>
+<a href="/finalcomm/sample/openBoardList.do">여행장터</a>
+<br><br>
+<a href="/finalcomm/sample/openBoardList.do">분실센터</a>
+<br><br>
+<a href="/finalcomm/sample/openBoardList.do">동행찾기</a>
+<br><br>
+<a href="/finalcomm/sample/openBoardList.do">피해사례</a>
+<br><br>
+
+</body>
+</html>
